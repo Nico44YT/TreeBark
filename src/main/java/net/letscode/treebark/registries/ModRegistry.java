820bb95a7fd807f2989ebac8abd03bf5acf6ba65
Nico44YT <Nico_44-YouTube@gmx.de>
2023-08-15 02:14:50 +0200

    Move files to 'treebark' package

---
+++ src/main/java/net/letscode/treebark/registries/ModRegistry.java
@@ -0,0 +1,27 @@
+package net.letscode.treebark.registries;
+
+import net.letscode.treebark.event.UseBlockHandler;
+import net.letscode.treebark.item.ModItems;
+import net.fabricmc.fabric.api.event.player.UseBlockCallback;
+import net.fabricmc.fabric.api.registry.FuelRegistry;
+
+public class ModRegistry {
+
+    public static void registerModFuels() {
+        FuelRegistry registry = FuelRegistry.INSTANCE;
+
+        registry.add(ModItems.ACACIA_BARK, 40);
+        registry.add(ModItems.BIRCH_BARK, 40);
+        registry.add(ModItems.CRIMSON_BARK, 60);
+        registry.add(ModItems.DARK_OAK_BARK, 40);
+        registry.add(ModItems.JUNGLE_BARK, 40);
+        registry.add(ModItems.MANGROVE_BARK, 40);
+        registry.add(ModItems.OAK_BARK, 40);
+        registry.add(ModItems.SPRUCE_BARK, 40);
+        registry.add(ModItems.WARPED_BARK, 60);
+    }
+
+    public static void registerEvents(){
+        UseBlockCallback.EVENT.register(new UseBlockHandler());
+    }
+}
